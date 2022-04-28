9b12b619ef9abc051e5dc848cb7b26a19633076c
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2022-04-28 22:50:17 +0200

    Add fab button for menu section

--- Desktop/Projects/React/reactts-baerum-trefelling-website/react-app/src/components/footer/Footer.styles.ts
+++ Desktop/Projects/React/reactts-baerum-trefelling-website/react-app/src/components/footer/Footer.styles.ts
@@ -7,7 +7,6 @@ export const Wrapper = styled.div`
   flex-direction: column;
   width: 100%;
   min-height: 8em;
-  padding: 1em;
   background-color: var(--color--primary);
   color: white;
 
@@ -18,5 +17,16 @@ export const Wrapper = styled.div`
 
   .footer__img {
     max-width: 8em;
+  }
+
+  .footer__mojo-websites-link {
+    width: 100%;
+    height: 2em;
+    background-color: var(--color--secondary);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1em 0 0 0;
+    bottom: 0;
   }
 `;
--- Desktop/Projects/React/reactts-baerum-trefelling-website/react-app/src/components/footer/Footer.tsx
+++ Desktop/Projects/React/reactts-baerum-trefelling-website/react-app/src/components/footer/Footer.tsx
@@ -11,6 +11,12 @@ const Footer = () => {
       <div className="footer__inner-container">
         Copyright Baerumtreefelling.no 2022
       </div>
+      <div className="footer__mojo-websites-link">
+        <a href="https://mojo-websites.no/">
+          {" "}
+          Website created by www.mojo-websites.no
+        </a>
+      </div>
     </Wrapper>
   );
 };
