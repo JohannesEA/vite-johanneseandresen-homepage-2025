f3f16cc44e78c9238165242f10980251267858f2
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2023-06-16 11:10:13 +0200

    feat: add sosial icons to footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa"; // Importing social icons
 import "./footer.css";
 import { ContactProps } from "../../hooks/useFetchContactinformation";
 
@@ -15,17 +15,39 @@ const Footer = ({ data }: FooterProps) => {
     <footer className="footer">
       <Container>
         <Row className="justify-content-between text-center text-md-start">
-          <Col md={4} className="footer__section">
+          <Col md={3} className="footer__section">
             <h4>Kontakt oss</h4>
             <p>{data.phonenumber}</p>
             <p>{data.email}</p>
             <p>{data.adress}</p>
           </Col>
-          <Col md={4} className="footer__section">
+          <Col md={3} className="footer__section">
             <h4>Om oss</h4>
             <p>Sesongbasert</p>
             <p>Kjærlighet</p>
             <p>Blomster</p>
+          </Col>
+          <Col md={3} className="footer__section ">
+            <h4>Følg oss</h4>
+            <div className="footer__icons">
+              <a
+                href={"https://www.facebook.com/skeivestilker"}
+                target="_blank"
+                rel="noreferrer"
+                className="footer__icon"
+              >
+                <FaFacebookF size={40} />
+              </a>
+
+              <a
+                href={"https://www.instagram.com/skeivestilker/"}
+                target="_blank"
+                rel="noreferrer"
+                className="footer__icon"
+              >
+                <FaInstagram size={40} />
+              </a>
+            </div>
           </Col>
         </Row>
         <div className="footer__copyright">
